1d6eef47e53b3ae2beba28e17b7a3752515bbdae
AnyiLS <tdprogramacioneml5@gmail.com>
2024-04-26 19:55:24 -0500

    firts commit final

--- src/screens/Medicines/Medicines.tsx
+++ src/screens/Medicines/Medicines.tsx
@@ -21,7 +21,7 @@ const Medicines: React.FC = (): JSX.Element => {
 						<svg
 							viewBox="0 0 1920 1080"
 							width="100%"
-							height={width < 1024 ? "100vh" : '100%'}
+							height={"100vh"}
 							preserveAspectRatio="none">
 							<defs>
 								<style>
